bc0276e4222e200936308b78e32d58a4b11fdb4a
ybilyk <Yaroslav.Bilyk@dataart.com>
2017-11-21 19:20:37 +0200

    Refactor

--- src/main/java/com/bilyk/App.java
+++ src/main/java/com/bilyk/App.java
@@ -1,14 +1,15 @@
 package com.bilyk;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        Triangle triangle = (Triangle) context.getBean("triangle-alias");
-        triangle.draw();
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        context.registerShutdownHook();
+        Triangle triangle1 = (Triangle) context.getBean("triangle1");
+        triangle1.draw();
     }
 }
--- src/main/java/com/bilyk/Triangle.java
+++ src/main/java/com/bilyk/Triangle.java
@@ -1,15 +1,31 @@
 package com.bilyk;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import java.util.List;
+
 import static java.lang.String.format;
 
-public class Triangle {
+public class Triangle implements InitializingBean, DisposableBean {
 
+    @InjectRandomPoint
     private Point pointA;
+
+    @InjectRandomPoint
     private Point pointB;
+
+    @InjectRandomPoint
     private Point pointC;
+
+    private List<Point> points;
 
     public void draw() {
         System.out.println(format("Drawing triangle with points: %s, %s, %s.", pointA, pointB, pointC));
+    }
+
+    public void drawList() {
+        System.out.println(points);
     }
 
     public Point getPointA() {
@@ -34,5 +50,36 @@ public class Triangle {
 
     public void setPointC(Point pointC) {
         this.pointC = pointC;
+    }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
+
+    @Override
+    public String toString() {
+        return "This is simple triangle object";
+    }
+
+    public void init() {
+        System.out.println("Global init method called for Triangle bean");
+    }
+
+    public void clean() {
+        System.out.println("Global destroy method called for Triangle bean");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitializingBean init method called for Triangle bean");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean destroy method called for Triangle bean");
     }
 }
