b02808186d04a7ebf092ca524537e762c8d42cd5
Yaroslav <bilyk.yaroslav.i@gmail.com>
2020-12-22 21:34:46 +0200

    f1

--- src/main/java/com/bilyk/AdvancedTriangle.java
+++ src/main/java/com/bilyk/AdvancedTriangle.java
@@ -4,6 +4,7 @@ public class AdvancedTriangle {
 // test comnebt master
     @Override
     public String toString() {
+        // test comment
         return "This is advanced triangle object";
     }
 }
